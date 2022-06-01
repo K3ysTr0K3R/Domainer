50b65f09a573a2ddf2998b0db34b1cf846b679c9
k3ystr0k3r <70909693+K3ysTr0K3R@users.noreply.github.com>
2022-06-01 17:36:34 -0400

    Update domainer.sh

--- domainer.sh
+++ domainer.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+
+clear
+
+echo -e "\e[93m -------------------------------------- "
+echo "| Coded By: K3ysTr0K3R                 |"
+echo "|                                      |"
+echo "| I am not responsible for any misuse  |"
+echo "| of this program.                     |"
+echo " -------------------------------------- "
+
+echo " ======== "
+echo "|Domainer|"
+echo " ======== "
+
+if [ -z "$1" ]; then
+	echo "[*] You Must Enter A domain NAME!"
+	exit 1
+fi
+
+assetfinder $1 > subdomains
+
+for domain in $(cat subdomains); do
+	if curl -s $domain &> /dev/null
+then
+	echo -e "\e[96m[*] $domain <------> {Online}"
+else
+	echo -e "\e[91m[~] $domain <------> {Offline}"
+fi
+done
+
+rm subdomains
+whoami=$(whoami)
+
+echo "[*] All Done $whoami, Thx for using Domainer"
