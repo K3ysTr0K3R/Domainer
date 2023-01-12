ce727b0c88fb43a0cfb8ad3313f848760ba0f8b9
k3ystr0k3r <70909693+K3ysTr0K3R@users.noreply.github.com>
2023-01-12 08:05:15 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-sudo apt get install assetfinder
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then 
+        echo "[!] Run the installation as root"
+        exit
+fi
+
+apt-get update
+apt-get install -y assetfinder
+
